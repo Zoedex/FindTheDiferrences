e792ae7ca13a061395f24bcbced5cc6340adb918
Lay <senglaypann@gmail.com>
2023-11-08 17:22:37 +0700

    fix white sapce and incorrection of finding difference

--- Find_defferrences_on_string.js
+++ Find_defferrences_on_string.js
@@ -5,16 +5,22 @@ const getValue = (id) => {
     return document.querySelector(`${id}`).value;
 }
 
+//compare array function
+const compareArrays = (array1, array2) => {
+    return JSON.stringify(array1) === JSON.stringify(array2);
+  };
+
 //push earch word to an array
 const splitField = (field, array) => {
-    let newArray = field.split(" ");
+    array.length = 0;
+    let newArray = field.split(/\s+/);
     array.push(...newArray);
     console.log('spit field ' , array);
 }
 
 
 //compare (arr1 > arr2)
-const generateResult = (array1,array2) => {
+const generateResult = (array1, array2, textClass) => {
     let dummyArr = array1.slice();
     let arr2Length = array2.length;
     console.log('generate')
@@ -26,57 +32,79 @@ const generateResult = (array1,array2) => {
             if (dummyArr[j] === array2[i]) {
                 dummyArr.splice(j,1);
                 j--;
+                break;
             }
         }
     }
 
     console.log(dummyArr)
+
     // start generating differrences
+    let resultLabel = '';
     let result = '';
     let count = 0;
-    for(let word in array1){
-        console.log(word)
-        if (array1[word] === dummyArr[count]) {
-            result += `<span>${array1[word]}</span> `;
-            count ++;
-        }else {
-            result += `${array1[word]} `;
-            
+
+    if (dummyArr.length === array1.length){
+        resultLabel = 'Result:';
+        result = 'These two text are completely different';
+    } else {
+        for(let word in array1){
+            console.log(word)
+            if (array1[word] === dummyArr[count]) {
+                result += `<span>${array1[word]}</span> `;
+                count ++;
+            }else {
+                result += `${array1[word]} `;
+                
+            }
         }
+        resultLabel = `Result: Here are what ${textClass} text missing.`
     }
     
-    
+    document.querySelector('#result-label').innerHTML = resultLabel;
     document.querySelector('#result').innerHTML = result;
 
 }
 
 // find 
 const find = () => {
+    const primaryText = 'primary';
+    const secondaryText = 'secondary';
     if (getValue('#input1') === '' || getValue('#input2') === '') {
         return
     } else {
         splitField(getValue('#input1'), input1);
         splitField(getValue('#input2'), input2);
 
-        if(input1.length >= input2.length){
-            generateResult(input1,input2);
-            document.querySelector('#result-label').innerHTML = `Result: From primary sentence.`
+        if (compareArrays(input1,input2)){
+            document.querySelector('#result-label').innerHTML = 'Result:';
+            document.querySelector('#result').innerHTML = 'There is no any differences.';
+
+        }else if(input1.length >= input2.length){
+            generateResult(input1,input2,secondaryText);
+            
         }else {
-            generateResult(input2,input1);
-            document.querySelector('#result-label').innerHTML = `Result: From secondary sentence.`
+            generateResult(input2,input1,primaryText);
+            
         }
 
     }
 }
 
+
 //clear function 
-const button = document.querySelector('.clear-btn');
-button.addEventListener('click', function() {
+
+const clear = () => {
     console.log('clicked');
     input1.length = 0;
     input2.length = 0;
+    document.querySelector('#result-label').innerHTML = 'Result:';
+    document.querySelector('#result').innerHTML = '';
+}
+
+const button = document.querySelector('.clear-btn');
+button.addEventListener('click', function() {
+    clear();
     document.querySelector('#input1').value = '';
     document.querySelector('#input2').value = '';
-
-    document.querySelector('#result').innerHTML = '';
 });
